face3e1466442758a7b9c75f643827f08907f522
Jawertae <2541716+Jawertae@users.noreply.github.com>
2017-09-20 06:13:57 -0400

    housekeeping

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
-and may not be redistributed without written permission.*/
-
-//Using SDL, SDL_image, standard IO, strings, and file streams
-//#include <SDL.h>
+//#include <SDL2/SDL.h>
+//#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <string>
