6ab0eba4706751f9b1bb1908607d9fa9a15a79c2
Jawertae <2541716+Jawertae@users.noreply.github.com>
2017-11-05 01:24:32 -0400

    ughhh

--- main.cpp
+++ main.cpp
@@ -132,3 +132,69 @@ printf("should be at gameloop \n");
 
 	return 0;
 }
+
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+//printf( "screen height: %s" , SCREEN_HEIGHT );
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+printf("sdl init \n");
+		//Set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+printf("window made? \n");
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_SOFTWARE || SDL_RENDERER_ACCELERATED || SDL_RENDERER_PRESENTVSYNC);
+			if( gRenderer == NULL )
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+printf("renderer created \n");
+				//Initialize renderer color
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+//				get_Scalar();
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+				//Initialize Font
+                                if( TTF_Init() == -1 )
+                                {
+                                    printf( "SDL_TTF could not Initialize! %s\n", TTF_GetError() );
+                                    success = false;
+                                }
+			}
+		}
+	}
+
+	return success;
+}
