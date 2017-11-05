586203a807cd613920dd5db841b4e4c2f8834771
Jawertae <2541716+Jawertae@users.noreply.github.com>
2017-11-05 01:38:22 -0500

    Revert "added (broken) jump and text engine"
    This reverts commit eca596d5071a2f14e852e952c8a070875f0cbf4e.
    
    Conflicts:
    	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
@@ -5,6 +6,170 @@
 #include "globals.h"
 #include "actor.h"
 #include "debug.h"
+=======
+//lol bitches
+
+//#include <SDL2/SDL.h>
+//#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+
+//The dimensions of the level
+const int LEVEL_WIDTH = 1280;
+const int LEVEL_HEIGHT = 960;
+
+//Tile constants
+const int TILE_WIDTH = 80;
+const int TILE_HEIGHT = 80;
+const int TOTAL_TILES = 192;
+const int TOTAL_TILE_SPRITES = 12;
+
+//The different tile sprites
+const int TILE_RED = 0;
+const int TILE_GREEN = 1;
+const int TILE_BLUE = 2;
+const int TILE_CENTER = 3;
+const int TILE_TOP = 4;
+const int TILE_TOPRIGHT = 5;
+const int TILE_RIGHT = 6;
+const int TILE_BOTTOMRIGHT = 7;
+const int TILE_BOTTOM = 8;
+const int TILE_BOTTOMLEFT = 9;
+const int TILE_LEFT = 10;
+const int TILE_TOPLEFT = 11;
+
+//Texture wrapper class
+class LTexture
+{
+	public:
+		//Initializes variables
+		LTexture();
+
+		//Deallocates memory
+		~LTexture();
+
+		//Loads image at specified path
+		bool loadFromFile( std::string path );
+
+		#ifdef _SDL_TTF_H
+		//Creates image from font string
+		bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
+		#endif
+
+		//Deallocates texture
+		void free();
+
+		//Set color modulation
+		void setColor( Uint8 red, Uint8 green, Uint8 blue );
+
+		//Set blending
+		void setBlendMode( SDL_BlendMode blending );
+
+		//Set alpha modulation
+		void setAlpha( Uint8 alpha );
+
+		//Renders texture at given point
+		void render( int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE );
+
+		//Gets image dimensions
+		int getWidth();
+		int getHeight();
+
+	private:
+		//The actual hardware texture
+		SDL_Texture* mTexture;
+
+		//Image dimensions
+		int mWidth;
+		int mHeight;
+};
+
+//The tile
+class Tile
+{
+    public:
+		//Initializes position and type
+		Tile( int x, int y, int tileType );
+
+		//Shows the tile
+		void render( SDL_Rect& camera );
+
+		//Get the tile type
+		int getType();
+
+		//Get the collision box
+		SDL_Rect getBox();
+
+    private:
+		//The attributes of the tile
+		SDL_Rect mBox;
+
+		//The tile type
+		int mType;
+};
+
+//The dot that will move around on the screen
+class Dot
+{
+    public:
+		//The dimensions of the dot
+		static const int DOT_WIDTH = 20;
+		static const int DOT_HEIGHT = 20;
+
+		//Maximum axis velocity of the dot
+		static const int DOT_VEL = 10;
+
+		//Initializes the variables
+		Dot();
+
+		//Takes key presses and adjusts the dot's velocity
+		void handleEvent( SDL_Event& e );
+
+		//Moves the dot and check collision against tiles
+		void move( Tile *tiles[] );
+
+		//Centers the camera over the dot
+		void setCamera( SDL_Rect& camera );
+
+		//Shows the dot on the screen
+		void render( SDL_Rect& camera );
+
+    private:
+
+		bool jumpLock;
+		static const int jumpVel = 15;
+
+		//Collision box of the dot
+		SDL_Rect mBox;
+
+		//The velocity of the dot
+		int mVelX, mVelY;
+};
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia( Tile* tiles[] );
+
+//Frees media and shuts down SDL
+void close( Tile* tiles[] );
+
+//Box collision detector
+bool checkCollision( SDL_Rect a, SDL_Rect b );
+
+//Checks collision box against set of tiles
+bool touchesWall( SDL_Rect box, Tile* tiles[] );
+
+//Sets tiles from tile map
+bool setTiles( Tile *tiles[] );
+>>>>>>> parent of eca596d... added (broken) jump and text engine
 
 
 int main( int argc, char* args[] )
@@ -16,12 +181,53 @@ int main( int argc, char* args[] )
 	}
 	else
 	{
+<<<<<<< HEAD
 		//printf("initialized \n");
 		//The level tiles
 		Tile* tileSet[ TOTAL_TILES ];
 		printf("Tileset Created %s\n");
 		//Load media
 		if( !loadMedia( tileSet , gRenderer) )
+=======
+		//Color key image
+		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+
+		//Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = loadedSurface->w;
+			mHeight = loadedSurface->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	//Return success
+	mTexture = newTexture;
+	return mTexture != NULL;
+}
+
+#ifdef _SDL_TTF_H
+bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
+{
+	//Get rid of preexisting texture
+	free();
+
+	//Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
+	if( textSurface != NULL )
+	{
+		//Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
+		if( mTexture == NULL )
+>>>>>>> parent of eca596d... added (broken) jump and text engine
 		{
 			printf( "Failed to load media!\n" );
 		}
@@ -34,6 +240,7 @@ int main( int argc, char* args[] )
 			//Event handler
 			SDL_Event e;
 
+<<<<<<< HEAD
 			//The dot that will be moving around on the screen
 			Dot dot(gRenderer);
 
@@ -60,6 +267,48 @@ int main( int argc, char* args[] )
 					//Handle input for the dot
 					dot.handleEvent( e );
 				}
+=======
+    //Initialize the velocity
+    mVelX = 0;
+    mVelY = 0;
+    jumpLock = 0;
+}
+
+void Dot::handleEvent( SDL_Event& e )
+{
+    //If a key was pressed
+	if( e.type == SDL_KEYDOWN && e.key.repeat == 0 )
+    {
+        //Adjust the velocity
+        switch( e.key.keysym.sym )
+        {
+            case SDLK_UP: mVelY -= DOT_VEL; break;
+            case SDLK_DOWN: mVelY += DOT_VEL; break;
+            case SDLK_LEFT: mVelX -= DOT_VEL; break;
+            case SDLK_RIGHT: mVelX += DOT_VEL; break;
+	    case SDLK_SPACE: if(!jumpLock) {mVelY -= jumpVel;jumpLock=1;} break;
+        }
+    }
+    //If a key was released
+    else if( e.type == SDL_KEYUP && e.key.repeat == 0 )
+    {
+        //Adjust the velocity
+        switch( e.key.keysym.sym )
+        {
+            case SDLK_UP: mVelY += DOT_VEL; break;
+            case SDLK_DOWN: mVelY -= DOT_VEL; break;
+            case SDLK_LEFT: mVelX += DOT_VEL; break;
+            case SDLK_RIGHT: mVelX -= DOT_VEL; break;
+	    case SDLK_SPACE: if(jumpLock) {mVelY += jumpVel;} break;
+        }
+    }
+}
+
+void Dot::move( Tile *tiles[] )
+{
+    //Move the dot left or right
+    mBox.x += mVelX;
+>>>>>>> parent of eca596d... added (broken) jump and text engine
 
 				//Move the dot
 				dot.move( tileSet );
@@ -70,6 +319,7 @@ int main( int argc, char* args[] )
 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 				SDL_RenderClear( gRenderer );
 
+<<<<<<< HEAD
 				//Render level
 				for( int i = 0; i < TOTAL_TILES; ++i )
 				{
@@ -83,6 +333,16 @@ int main( int argc, char* args[] )
 
 				//Update screen
 				SDL_RenderPresent( gRenderer );
+=======
+    //If the dot went too far up or down or touched a wall
+    if( ( mBox.y < 0 ) || ( mBox.y + DOT_HEIGHT > LEVEL_HEIGHT ) || touchesWall( mBox, tiles ) )
+    {
+        //move back
+	jumpLock = 0;
+        mBox.y -= mVelY;
+    }
+}
+>>>>>>> parent of eca596d... added (broken) jump and text engine
 
 			}
 		}
@@ -147,6 +407,7 @@ bool init()
 					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
 					success = false;
 				}
+<<<<<<< HEAD
 				//Initialize Font
                                 if( TTF_Init() == -1 )
                                 {
@@ -159,3 +420,345 @@ bool init()
 	printf("init done \n");
 	return success;
 }
+=======
+			}
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia( Tile* tiles[] )
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load dot texture
+	if( !gDotTexture.loadFromFile( "assets/dot.bmp" ) )
+	{
+		printf( "Failed to load dot texture!\n" );
+		success = false;
+	}
+
+	//Load tile texture
+	if( !gTileTexture.loadFromFile( "assets/tiles.png" ) )
+	{
+		printf( "Failed to load tile set texture!\n" );
+		success = false;
+	}
+
+	//Load tile map
+	if( !setTiles( tiles ) )
+	{
+		printf( "Failed to load tile set!\n" );
+		success = false;
+	}
+
+	return success;
+}
+
+void close( Tile* tiles[] )
+{
+	//Deallocate tiles
+	for( int i = 0; i < TOTAL_TILES; ++i )
+	{
+		 if( tiles[ i ] == NULL )
+		 {
+			delete tiles[ i ];
+			tiles[ i ] = NULL;
+		 }
+	}
+
+	//Free loaded images
+	gDotTexture.free();
+	gTileTexture.free();
+
+	//Destroy window
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+bool checkCollision( SDL_Rect a, SDL_Rect b )
+{
+    //The sides of the rectangles
+    int leftA, leftB;
+    int rightA, rightB;
+    int topA, topB;
+    int bottomA, bottomB;
+
+    //Calculate the sides of rect A
+    leftA = a.x;
+    rightA = a.x + a.w;
+    topA = a.y;
+    bottomA = a.y + a.h;
+
+    //Calculate the sides of rect B
+    leftB = b.x;
+    rightB = b.x + b.w;
+    topB = b.y;
+    bottomB = b.y + b.h;
+
+    //If any of the sides from A are outside of B
+    if( bottomA <= topB )
+    {
+        return false;
+    }
+
+    if( topA >= bottomB )
+    {
+        return false;
+    }
+
+    if( rightA <= leftB )
+    {
+        return false;
+    }
+
+    if( leftA >= rightB )
+    {
+        return false;
+    }
+
+    //If none of the sides from A are outside B
+    return true;
+}
+
+bool setTiles( Tile* tiles[] )
+{
+	//Success flag
+	bool tilesLoaded = true;
+
+    //The tile offsets
+    int x = 0, y = 0;
+
+    //Open the map
+    std::ifstream map( "assets/lazy.map" );
+
+    //If the map couldn't be loaded
+    if( map == NULL )
+    {
+		printf( "Unable to load map file!\n" );
+		tilesLoaded = false;
+    }
+	else
+	{
+		//Initialize the tiles
+		for( int i = 0; i < TOTAL_TILES; ++i )
+		{
+			//Determines what kind of tile will be made
+			int tileType = -1;
+
+			//Read tile from map file
+			map >> tileType;
+
+			//If the was a problem in reading the map
+			if( map.fail() )
+			{
+				//Stop loading map
+				printf( "Error loading map: Unexpected end of file!\n" );
+				tilesLoaded = false;
+				break;
+			}
+
+			//If the number is a valid tile number
+			if( ( tileType >= 0 ) && ( tileType < TOTAL_TILE_SPRITES ) )
+			{
+				tiles[ i ] = new Tile( x, y, tileType );
+			}
+			//If we don't recognize the tile type
+			else
+			{
+				//Stop loading map
+				printf( "Error loading map: Invalid tile type at %d!\n", i );
+				tilesLoaded = false;
+				break;
+			}
+
+			//Move to next tile spot
+			x += TILE_WIDTH;
+
+			//If we've gone too far
+			if( x >= LEVEL_WIDTH )
+			{
+				//Move back
+				x = 0;
+
+				//Move to the next row
+				y += TILE_HEIGHT;
+			}
+		}
+
+		//Clip the sprite sheet
+		if( tilesLoaded )
+		{
+			gTileClips[ TILE_RED ].x = 0;
+			gTileClips[ TILE_RED ].y = 0;
+			gTileClips[ TILE_RED ].w = TILE_WIDTH;
+			gTileClips[ TILE_RED ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_GREEN ].x = 0;
+			gTileClips[ TILE_GREEN ].y = 80;
+			gTileClips[ TILE_GREEN ].w = TILE_WIDTH;
+			gTileClips[ TILE_GREEN ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_BLUE ].x = 0;
+			gTileClips[ TILE_BLUE ].y = 160;
+			gTileClips[ TILE_BLUE ].w = TILE_WIDTH;
+			gTileClips[ TILE_BLUE ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_TOPLEFT ].x = 80;
+			gTileClips[ TILE_TOPLEFT ].y = 0;
+			gTileClips[ TILE_TOPLEFT ].w = TILE_WIDTH;
+			gTileClips[ TILE_TOPLEFT ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_LEFT ].x = 80;
+			gTileClips[ TILE_LEFT ].y = 80;
+			gTileClips[ TILE_LEFT ].w = TILE_WIDTH;
+			gTileClips[ TILE_LEFT ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_BOTTOMLEFT ].x = 80;
+			gTileClips[ TILE_BOTTOMLEFT ].y = 160;
+			gTileClips[ TILE_BOTTOMLEFT ].w = TILE_WIDTH;
+			gTileClips[ TILE_BOTTOMLEFT ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_TOP ].x = 160;
+			gTileClips[ TILE_TOP ].y = 0;
+			gTileClips[ TILE_TOP ].w = TILE_WIDTH;
+			gTileClips[ TILE_TOP ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_CENTER ].x = 160;
+			gTileClips[ TILE_CENTER ].y = 80;
+			gTileClips[ TILE_CENTER ].w = TILE_WIDTH;
+			gTileClips[ TILE_CENTER ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_BOTTOM ].x = 160;
+			gTileClips[ TILE_BOTTOM ].y = 160;
+			gTileClips[ TILE_BOTTOM ].w = TILE_WIDTH;
+			gTileClips[ TILE_BOTTOM ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_TOPRIGHT ].x = 240;
+			gTileClips[ TILE_TOPRIGHT ].y = 0;
+			gTileClips[ TILE_TOPRIGHT ].w = TILE_WIDTH;
+			gTileClips[ TILE_TOPRIGHT ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_RIGHT ].x = 240;
+			gTileClips[ TILE_RIGHT ].y = 80;
+			gTileClips[ TILE_RIGHT ].w = TILE_WIDTH;
+			gTileClips[ TILE_RIGHT ].h = TILE_HEIGHT;
+
+			gTileClips[ TILE_BOTTOMRIGHT ].x = 240;
+			gTileClips[ TILE_BOTTOMRIGHT ].y = 160;
+			gTileClips[ TILE_BOTTOMRIGHT ].w = TILE_WIDTH;
+			gTileClips[ TILE_BOTTOMRIGHT ].h = TILE_HEIGHT;
+		}
+	}
+
+    //Close the file
+    map.close();
+
+    //If the map was loaded fine
+    return tilesLoaded;
+}
+
+bool touchesWall( SDL_Rect box, Tile* tiles[] )
+{
+    //Go through the tiles
+    for( int i = 0; i < TOTAL_TILES; ++i )
+    {
+        //If the tile is a wall type tile
+        if( ( tiles[ i ]->getType() >= TILE_CENTER ) && ( tiles[ i ]->getType() <= TILE_TOPLEFT ) )
+        {
+            //If the collision box touches the wall tile
+            if( checkCollision( box, tiles[ i ]->getBox() ) )
+            {
+                return true;
+            }
+        }
+    }
+
+    //If no wall tiles were touched
+    return false;
+}
+
+int main( int argc, char* args[] )
+{
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//The level tiles
+		Tile* tileSet[ TOTAL_TILES ];
+
+		//Load media
+		if( !loadMedia( tileSet ) )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//The dot that will be moving around on the screen
+			Dot dot;
+
+			//Level camera
+			SDL_Rect camera = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+
+			//While application is running
+			while( !quit )
+			{
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					//User requests quit
+					if( e.type == SDL_QUIT || e.key.keysym.sym == SDLK_q )
+					{
+						quit = true;
+					}
+
+					//Handle input for the dot
+					dot.handleEvent( e );
+				}
+
+				//Move the dot
+				dot.move( tileSet );
+				dot.setCamera( camera );
+
+				//Clear screen
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_RenderClear( gRenderer );
+
+				//Render level
+				for( int i = 0; i < TOTAL_TILES; ++i )
+				{
+					tileSet[ i ]->render( camera );
+				}
+
+				//Render dot
+				dot.render( camera );
+
+				//Update screen
+				SDL_RenderPresent( gRenderer );
+			}
+		}
+
+		//Free resources and close SDL
+		close( tileSet );
+	}
+
+	return 0;
+}
+>>>>>>> parent of eca596d... added (broken) jump and text engine
